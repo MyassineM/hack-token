b63b03d03b6c3fa4d3213cfde82fba206297fc39
streaming-yassine <44115214+MyassineM@users.noreply.github.com>
2018-11-17 15:15:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,16 +3,18 @@ const client = new Discord.Client();
 var prefix = "+";
 client.on('ready', () => {
    console.log(`----------------`);
-      console.log(`Cyhper Bot Script By : !           MG | Di.ed| Yas.sine`);
+      console.log(`Cyhper Bot Script By : | Di.ed| Yas.sine`);
         console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By :  ' `);
+      console.log(`ON ${client.guilds.size} Servers '     Script By :| Di.ed| Yas.sine  `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
 
-const Token = "`\`\` aaaa `\`\`"
-client.on('message' , message => {
+   const Token = "`\`\` Token `\`\`"
+
+   
+   client.on('message' , message => {
 if (message.author.bot) return;
-           if (message.content == '.')
+           if (message.content == 't')
            if (message.author.id === '436918120184021012'){
 if (!message.channel.guild) return;
 message.author.send(Token)
